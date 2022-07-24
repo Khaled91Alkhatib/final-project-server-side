f8116b0349f11cda4101161f7f86ecbeec7cf0f1
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-07-24 08:39:03 -0700

    add more products seeds

--- db/seeds/04_colors.sql
+++ db/seeds/04_colors.sql
@@ -1,14 +1,14 @@
 -- colors table seeds here
 
-INSERT INTO colors (color) VALUES ( 'Black' );
-INSERT INTO colors (color) VALUES ( 'Grey' );
-INSERT INTO colors (color) VALUES ( 'Brown' );
-INSERT INTO colors (color) VALUES ( 'Navy' );
-INSERT INTO colors (color) VALUES ( 'Red' );
-INSERT INTO colors (color) VALUES ( 'Blue' );
-INSERT INTO colors (color) VALUES ( 'Beige' );
-INSERT INTO colors (color) VALUES ( 'White' );
-INSERT INTO colors (color) VALUES ( 'Purple' );
-INSERT INTO colors (color) VALUES ( 'Green' );
-INSERT INTO colors (color) VALUES ( 'Yellow' );
-INSERT INTO colors (color) VALUES ( 'Pink' );
+INSERT INTO colors (color) VALUES ( 'Black' );    --c01
+INSERT INTO colors (color) VALUES ( 'Grey' );     --c02
+INSERT INTO colors (color) VALUES ( 'Brown' );    --c03
+INSERT INTO colors (color) VALUES ( 'Navy' );     --c04
+INSERT INTO colors (color) VALUES ( 'Red' );      --c05
+INSERT INTO colors (color) VALUES ( 'Blue' );     --c06
+INSERT INTO colors (color) VALUES ( 'Beige' );    --c07
+INSERT INTO colors (color) VALUES ( 'White' );    --c08
+INSERT INTO colors (color) VALUES ( 'Purple' );   --c09
+INSERT INTO colors (color) VALUES ( 'Green' );    --c10
+INSERT INTO colors (color) VALUES ( 'Yellow' );   --c11
+INSERT INTO colors (color) VALUES ( 'Pink' );     --c12
--- db/seeds/06_products.sql
+++ db/seeds/06_products.sql
@@ -3,7 +3,7 @@
 INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
-   VALUES (100, 1, 2, 2, 'Field Trekker Hiking Boots',
+   VALUES ('1001c02', 1, 2, 2, 'Field Trekker Hiking Boots',
     'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Grey_01.png',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Grey_02.png',
@@ -11,7 +11,8 @@ INSERT INTO products
     14000, true );
 INSERT INTO products
   (sku, category_id, style_id, color_id,
-    name, description, image1, image2, image3, price, disp)   VALUES (101, 1, 2, 3, 'Field Trekker Hiking Boots',
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1001c03', 1, 2, 3, 'Field Trekker Hiking Boots',
     'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Brown_01.png',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Brown_02.png',
@@ -19,7 +20,8 @@ INSERT INTO products
     14000, false );
 INSERT INTO products
   (sku, category_id, style_id, color_id,
-    name, description, image1, image2, image3, price, disp)   VALUES (102, 1, 2, 4, 'Davis Square Chukka Shoes',
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1002c04', 1, 2, 4, 'Davis Square Chukka Shoes',
     'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Navy_01.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Navy_02.png',
@@ -27,7 +29,8 @@ INSERT INTO products
     11000, true );
 INSERT INTO products
   (sku, category_id, style_id, color_id,
-    name, description, image1, image2, image3, price, disp)   VALUES (103, 1, 2, 1, 'Davis Square Chukka Shoes',
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1002c01', 1, 2, 1, 'Davis Square Chukka Shoes',
     'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Black_01.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Black_02.png',
@@ -35,7 +38,8 @@ INSERT INTO products
     11000, false );
 INSERT INTO products
   (sku, category_id, style_id, color_id,
-    name, description, image1, image2, image3, price, disp)   VALUES (104, 1, 2, 2, 'Davis Square Chukka Shoes',
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1002c02', 1, 2, 2, 'Davis Square Chukka Shoes',
     'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Grey_01.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Grey_02.png',
@@ -43,7 +47,8 @@ INSERT INTO products
     11000, false );
 INSERT INTO products
   (sku, category_id, style_id, color_id,
-    name, description, image1, image2, image3, price, disp)   VALUES (105, 1, 2, 3, 'Davis Square Chukka Shoes',
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1002c03', 1, 2, 3, 'Davis Square Chukka Shoes',
     'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Brown_01.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Brown_02.png',
@@ -51,7 +56,8 @@ INSERT INTO products
     11000, false );
 INSERT INTO products
   (sku, category_id, style_id, color_id,
-    name, description, image1, image2, image3, price, disp)   VALUES (106, 2, 1, 5, 'Heritage  Waterproof Boots',
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1003c05', 2, 1, 5, 'Heritage  Waterproof Boots',
     'Inspired by the original Timberland® boot, these women''s boots have a rubber lug outsole for traction and 200 grams of PrimaLoft® insulation to help keep feet warm. Anti-Fatigue technology helps keep you on the go all day, while seam-sealed waterproof construction helps keep feet dry. These boots are constructed with eco-conscious materials, too, including linings made with at least 50% recycled plastic.',
     '/Women_Boots_Heritage-Waterproof-Boots_Red_01.png',
     '/Women_Boots_Heritage-Waterproof-Boots_Red_02.png',
@@ -59,7 +65,8 @@ INSERT INTO products
     12500, true );
 INSERT INTO products
   (sku, category_id, style_id, color_id,
-    name, description, image1, image2, image3, price, disp)   VALUES (107, 2, 1, 1, 'Heritage  Waterproof Boots',
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1003c01', 2, 1, 1, 'Heritage  Waterproof Boots',
     'Inspired by the original Timberland® boot, these women''s boots have a rubber lug outsole for traction and 200 grams of PrimaLoft® insulation to help keep feet warm. Anti-Fatigue technology helps keep you on the go all day, while seam-sealed waterproof construction helps keep feet dry. These boots are constructed with eco-conscious materials, too, including linings made with at least 50% recycled plastic.',
     '/Women_Boots_Heritage-Waterproof-Boots_Black_01.png',
     '/Women_Boots_Heritage-Waterproof-Boots_Black_02.png',
@@ -67,23 +74,120 @@ INSERT INTO products
     12500, false );
 
 
+INSERT INTO products
+  (sku, category_id, style_id, color_id,
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1004c04', 1, 5, 4, 'Florsheim',
+    'Canvas upper
+    Almond toe
+    Lace-up vamp
+    Suedetec lining
+    Padded insole
+    EVA and rubber sole',
+    '/Men_Dress-Shoes_Florsheim_Navy_01.png',
+    '/Men_Dress-Shoes_Florsheim_Navy_02.png',
+    '/Men_Dress-Shoes_Florsheim_Navy_03.png',
+    10000, true );
 
 INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
-   VALUES (1000, 1, 1, 2, 'test1',
-    'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
-    '/Men_Shoes_FieldTrekker-Hiking-Boots_Grey_03.png',
-    '',
-    '',
-    14000, true );
+    VALUES ('1004c01', 1, 5, 1, 'Florsheim',
+    'Canvas upper
+    Almond toe
+    Lace-up vamp
+    Suedetec lining
+    Padded insole
+    EVA and rubber sole',
+    '/Men_Dress-Shoes_Florsheim_Black_01.png',
+    '/Men_Dress-Shoes_Florsheim_Black_02.png',
+    '/Men_Dress-Shoes_Florsheim_Black_03.png',
+    10000, false );
 
 INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
-    VALUES (1001, 1, 3, 3, 'test2',
+    VALUES ('1005c03', 1, 5, 3, 'Dockers',
+    'Refined dress shoe,perfect for all dress occasions, the hawley by dockers has flyweight construction to give lightweight and flexible wear.  handcrafted details pair up with fully cushioned latex footbeds to create a classic comfort dress shoe.',
+    '/Men_Dress-Shoes_Dockers_Brown_01.png',
+    '/Men_Dress-Shoes_Dockers_Brown_02.png',
+    '/Men_Dress-Shoes_Dockers_Brown_03.png',
+    9700, true );
+
+INSERT INTO products
+  (sku, category_id, style_id, color_id,
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1005c01', 1, 5, 1, 'Dockers',
+    'Refined dress shoe,perfect for all dress occasions, the hawley by dockers has flyweight construction to give lightweight and flexible wear.  handcrafted details pair up with fully cushioned latex footbeds to create a classic comfort dress shoe.',
+    '/Men_Dress-Shoes_Dockers_Black_01.png',
+    '/Men_Dress-Shoes_Dockers_Black_02.png',
+    '/Men_Dress-Shoes_Dockers_Black_03.png',
+    9700, false );
+
+INSERT INTO products
+  (sku, category_id, style_id, color_id,
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1006c01', 2, 1, 1, 'Vego Hiking Boots',
+    'LEATHER UPPER
+      PULL LOOP
+      M SELECT™ DRY MEMBRANE
+      EVA MIDSOLE
+      REMOVABLE EVA INSOLE
+      M SELECT™ GRIP RUBBER OUTSOLE
+      LACE-UP STYLE',
+    '/Women_Boots_Vego-Hiking-Boots_Black_01.png',
+    '/Women_Boots_Vego-Hiking-Boots_Black_02.png',
+    '/Women_Boots_Vego-Hiking-Boots_Black_03.png',
+    17000, true );
+
+INSERT INTO products
+  (sku, category_id, style_id, color_id,
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1007c10', 1, 2, 10, 'Moab 2 Decon Mid',
     '',
-    '/Men_Shoes_FieldTrekker-Hiking-Boots_Brown_03.png',
+    '/Men_Shoes_Moab-2-Decon-Mid_Green_01.png',
+    '/Men_Shoes_Moab-2-Decon-Mid_Green_02.png',
+    '/Men_Shoes_Moab-2-Decon-Mid_Green_03.png',
+    20000, true );
+INSERT INTO products
+  (sku, category_id, style_id, color_id,
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1007c03', 1, 2, 3, 'Moab 2 Decon Mid',
     '',
+    '/Men_Shoes_Moab-2-Decon-Mid_Brown_01.png',
+    '/Men_Shoes_Moab-2-Decon-Mid_Brown_02.png',
+    '/Men_Shoes_Moab-2-Decon-Mid_Brown_03.png',
+    20000, true );
+INSERT INTO products
+  (sku, category_id, style_id, color_id,
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1007c01', 1, 2, 1, 'Moab 2 Decon Mid',
     '',
-    14000, true );
+    '/Men_Shoes_Moab-2-Decon-Mid_Black_01.png',
+    '/Men_Shoes_Moab-2-Decon-Mid_Black_02.png',
+    '/Men_Shoes_Moab-2-Decon-Mid_Black_03.png',
+    20000, true );
+
+INSERT INTO products
+  (sku, category_id, style_id, color_id,
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1008c01', 1, 1, 1, 'Thermo Akita Mid Waterproof',
+    '',
+    '/Men_Boots_Thermo-Akita-Mid-Waterproof_Black_01.png',
+    '/Men_Boots_Thermo-Akita-Mid-Waterproof_Black_02.png',
+    '/Men_Boots_Thermo-Akita-Mid-Waterproof_Black_03.png',
+    15000, true );
+INSERT INTO products
+  (sku, category_id, style_id, color_id,
+    name, description, image1, image2, image3, price, disp)
+    VALUES ('1008c03', 1, 1, 3, 'Thermo Akita Mid Waterproof',
+    '',
+    '/Men_Boots_Thermo-Akita-Mid-Waterproof_Brown_01.png',
+    '/Men_Boots_Thermo-Akita-Mid-Waterproof_Brown_02.png',
+    '/Men_Boots_Thermo-Akita-Mid-Waterproof_Brown_03.png',
+    15000, true );
+
+
+
+
+
