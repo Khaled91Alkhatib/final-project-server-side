499cb29004d431165c18c8eb29046a774bc5764c
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-07-26 19:33:31 -0700

    update description of products \n

--- db/seeds/06_products.sql
+++ db/seeds/06_products.sql
@@ -4,7 +4,7 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
    VALUES ('1001c02', 1, 2, 2, 'Field Trekker Hiking Boots',
-    'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
+    E'Waterproof: protects against rain, puddles and slush to keep feet dry in wet conditions\nTrail: provides superior stability and traction on hills, uneven surfaces and changing terrain\nFUTURELIGHT™ breathable, waterproof membrane keeps out the wet elements\nOrthoLite® footbed\nTextile and synthetic upper/textile lining/synthetic sole',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Grey_01.png',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Grey_02.png',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Grey_03.png',
@@ -13,16 +13,17 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1001c03', 1, 2, 3, 'Field Trekker Hiking Boots',
-    'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
+    E'Waterproof: protects against rain, puddles and slush to keep feet dry in wet conditions\nTrail: provides superior stability and traction on hills, uneven surfaces and changing terrain\nFUTURELIGHT™ breathable, waterproof membrane keeps out the wet elements\nOrthoLite® footbed\nTextile and synthetic upper/textile lining/synthetic sole',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Brown_01.png',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Brown_02.png',
     '/Men_Shoes_FieldTrekker-Hiking-Boots_Brown_03.png',
     14000, false );
+
 INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1002c04', 1, 2, 4, 'Davis Square Chukka Shoes',
-    'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
+    E'Trail: provides superior stability and traction on hills, uneven surfaces and changing terrain\nRemovable, cushioned insole with arch support\nLeather and textile upper/recycled synthetic lining/rubber sole',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Navy_01.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Navy_02.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Navy_03.png',
@@ -31,7 +32,7 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1002c01', 1, 2, 1, 'Davis Square Chukka Shoes',
-    'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
+    E'Trail: provides superior stability and traction on hills, uneven surfaces and changing terrain\nRemovable, cushioned insole with arch support\nLeather and textile upper/recycled synthetic lining/rubber sole',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Black_01.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Black_02.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Black_03.png',
@@ -40,7 +41,7 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1002c02', 1, 2, 2, 'Davis Square Chukka Shoes',
-    'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
+    E'Trail: provides superior stability and traction on hills, uneven surfaces and changing terrain\nRemovable, cushioned insole with arch support\nLeather and textile upper/recycled synthetic lining/rubber sole',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Grey_01.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Grey_02.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Grey_03.png',
@@ -49,16 +50,17 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1002c03', 1, 2, 3, 'Davis Square Chukka Shoes',
-    'These men''s hikers were designed for comfort with a cushiony EVA-blend foam midsole and lightweight OrthoLite® footbed. Added features include a convenient speed-lacing system and eco-conscious materials',
+    E'Trail: provides superior stability and traction on hills, uneven surfaces and changing terrain\nRemovable, cushioned insole with arch support\nLeather and textile upper/recycled synthetic lining/rubber sole',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Brown_01.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Brown_02.png',
     '/Men_Shoes_Davis-Square-Chukka-Shoes_Brown_03.png',
     11000, false );
+
 INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1003c05', 2, 1, 5, 'Heritage  Waterproof Boots',
-    'Inspired by the original Timberland® boot, these women''s boots have a rubber lug outsole for traction and 200 grams of PrimaLoft® insulation to help keep feet warm. Anti-Fatigue technology helps keep you on the go all day, while seam-sealed waterproof construction helps keep feet dry. These boots are constructed with eco-conscious materials, too, including linings made with at least 50% recycled plastic.',
+    E'Waterproof seam-sealed construction helps keep feet dry\nDirect-attach construction for durability\n200 grams of PrimaLoft insulation\nPadded collar for a comfortable fit\nRemovable, antifatigue insole\nRubber lug outsole for traction and durability is made with 10% plant-based materials\nLeather upper/leather and textile lining/rubber sole\nOne or more major components use at least 50% leather sourced from a tannery that has achieved a Leather Working Group Silver rating for energy use, waste production and water treatment',
     '/Women_Boots_Heritage-Waterproof-Boots_Red_01.png',
     '/Women_Boots_Heritage-Waterproof-Boots_Red_02.png',
     '/Women_Boots_Heritage-Waterproof-Boots_Red_03.png',
@@ -67,38 +69,26 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1003c01', 2, 1, 1, 'Heritage  Waterproof Boots',
-    'Inspired by the original Timberland® boot, these women''s boots have a rubber lug outsole for traction and 200 grams of PrimaLoft® insulation to help keep feet warm. Anti-Fatigue technology helps keep you on the go all day, while seam-sealed waterproof construction helps keep feet dry. These boots are constructed with eco-conscious materials, too, including linings made with at least 50% recycled plastic.',
+    E'Waterproof seam-sealed construction helps keep feet dry\nDirect-attach construction for durability\n200 grams of PrimaLoft insulation\nPadded collar for a comfortable fit\nRemovable, antifatigue insole\nRubber lug outsole for traction and durability is made with 10% plant-based materials\nLeather upper/leather and textile lining/rubber sole\nOne or more major components use at least 50% leather sourced from a tannery that has achieved a Leather Working Group Silver rating for energy use, waste production and water treatment',
     '/Women_Boots_Heritage-Waterproof-Boots_Black_01.png',
     '/Women_Boots_Heritage-Waterproof-Boots_Black_02.png',
     '/Women_Boots_Heritage-Waterproof-Boots_Black_03.png',
     12500, false );
 
-
 INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1004c04', 1, 5, 4, 'Florsheim',
-    'Canvas upper
-    Almond toe
-    Lace-up vamp
-    Suedetec lining
-    Padded insole
-    EVA and rubber sole',
+    E'Canvas upper\nAlmond toe\nLace-up vamp\nSuedetec lining\nPadded insole\nEVA and rubber sole',
     '/Men_Dress-Shoes_Florsheim_Navy_01.png',
     '/Men_Dress-Shoes_Florsheim_Navy_02.png',
     '/Men_Dress-Shoes_Florsheim_Navy_03.png',
     10000, true );
-
 INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1004c01', 1, 5, 1, 'Florsheim',
-    'Canvas upper
-    Almond toe
-    Lace-up vamp
-    Suedetec lining
-    Padded insole
-    EVA and rubber sole',
+    E'Canvas upper\nAlmond toe\nLace-up vamp\nSuedetec lining\nPadded insole\nEVA and rubber sole',
     '/Men_Dress-Shoes_Florsheim_Black_01.png',
     '/Men_Dress-Shoes_Florsheim_Black_02.png',
     '/Men_Dress-Shoes_Florsheim_Black_03.png',
@@ -108,17 +98,16 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1005c03', 1, 5, 3, 'Dockers',
-    'Refined dress shoe,perfect for all dress occasions, the hawley by dockers has flyweight construction to give lightweight and flexible wear.  handcrafted details pair up with fully cushioned latex footbeds to create a classic comfort dress shoe.',
+    E'Refined dress shoe,perfect for all dress occasions, the hawley by dockers has flyweight construction to give lightweight and flexible wear.\nhandcrafted details pair up with fully cushioned latex footbeds to create a classic comfort dress shoe.',
     '/Men_Dress-Shoes_Dockers_Brown_01.png',
     '/Men_Dress-Shoes_Dockers_Brown_02.png',
     '/Men_Dress-Shoes_Dockers_Brown_03.png',
     9700, true );
-
 INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1005c01', 1, 5, 1, 'Dockers',
-    'Refined dress shoe,perfect for all dress occasions, the hawley by dockers has flyweight construction to give lightweight and flexible wear.  handcrafted details pair up with fully cushioned latex footbeds to create a classic comfort dress shoe.',
+    E'Refined dress shoe,perfect for all dress occasions, the hawley by dockers has flyweight construction to give lightweight and flexible wear.\nhandcrafted details pair up with fully cushioned latex footbeds to create a classic comfort dress shoe.',
     '/Men_Dress-Shoes_Dockers_Black_01.png',
     '/Men_Dress-Shoes_Dockers_Black_02.png',
     '/Men_Dress-Shoes_Dockers_Black_03.png',
@@ -128,13 +117,7 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1006c01', 2, 1, 1, 'Vego Hiking Boots',
-    'LEATHER UPPER
-      PULL LOOP
-      M SELECT™ DRY MEMBRANE
-      EVA MIDSOLE
-      REMOVABLE EVA INSOLE
-      M SELECT™ GRIP RUBBER OUTSOLE
-      LACE-UP STYLE',
+    E'LEATHER UPPER\nPULL LOOP\nM SELECT™ DRY MEMBRANE\nEVA MIDSOLE\nREMOVABLE EVA INSOLE\nM SELECT™ GRIP RUBBER OUTSOLE\nLACE-UP STYLE',
     '/Women_Boots_Vego-Hiking-Boots_Black_01.png',
     '/Women_Boots_Vego-Hiking-Boots_Black_02.png',
     '/Women_Boots_Vego-Hiking-Boots_Black_03.png',
@@ -144,7 +127,7 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1007c10', 1, 2, 10, 'Moab 2 Decon Mid',
-    '',
+    E'Full grain leather upper\n100% recycled laces and webbing\nCanvas lining\n100% recycled mesh lined removable PU insole\nMerrell Air Cushion in the heel absorbs shock and adds stability\nMolded nylon arch shank\nLightweight EVA foam midsole for stability and comfort\nVibram® TC5+ rubber sole\n2lbs-7oz, 1080g',
     '/Men_Shoes_Moab-2-Decon-Mid_Green_01.png',
     '/Men_Shoes_Moab-2-Decon-Mid_Green_02.png',
     '/Men_Shoes_Moab-2-Decon-Mid_Green_03.png',
@@ -153,7 +136,7 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1007c03', 1, 2, 3, 'Moab 2 Decon Mid',
-    '',
+    E'Full grain leather upper\n100% recycled laces and webbing\nCanvas lining\n100% recycled mesh lined removable PU insole\nMerrell Air Cushion in the heel absorbs shock and adds stability\nMolded nylon arch shank\nLightweight EVA foam midsole for stability and comfort\nVibram® TC5+ rubber sole\n2lbs-7oz, 1080g',
     '/Men_Shoes_Moab-2-Decon-Mid_Brown_01.png',
     '/Men_Shoes_Moab-2-Decon-Mid_Brown_02.png',
     '/Men_Shoes_Moab-2-Decon-Mid_Brown_03.png',
@@ -162,7 +145,7 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1007c01', 1, 2, 1, 'Moab 2 Decon Mid',
-    '',
+    E'Full grain leather upper\n100% recycled laces and webbing\nCanvas lining\n100% recycled mesh lined removable PU insole\nMerrell Air Cushion in the heel absorbs shock and adds stability\nMolded nylon arch shank\nLightweight EVA foam midsole for stability and comfort\nVibram® TC5+ rubber sole\n2lbs-7oz, 1080g',
     '/Men_Shoes_Moab-2-Decon-Mid_Black_01.png',
     '/Men_Shoes_Moab-2-Decon-Mid_Black_02.png',
     '/Men_Shoes_Moab-2-Decon-Mid_Black_03.png',
@@ -172,7 +155,7 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1008c01', 1, 1, 1, 'Thermo Akita Mid Waterproof',
-    '',
+    E'Waterproof membrane seals out water and lets moisture escape\nWaterproof coated leather and mesh upper\nMetal hook traditional lace closure\nBellows tongue keeps out debris\nGaiter D-Ring\nBreathable mesh lining\n200 grams lightweight, low bulk insulation keeps you comfortable\nFleece collar\nKinetic Fit™ BASE removable contoured insole for flexible support\nMerrell Air Cushion in the heel absorbs shock and adds stability\nMolded nylon arch shank\nLightweight EVA foam midsole for stability and comfort\nMerrell sticky rubber outsole tuned for winter optimizes traction over cold weather surface conditions\nLug Depth: 4.0mm\nWeight: 2LBS-5OZ/1028.0G',
     '/Men_Boots_Thermo-Akita-Mid-Waterproof_Black_01.png',
     '/Men_Boots_Thermo-Akita-Mid-Waterproof_Black_02.png',
     '/Men_Boots_Thermo-Akita-Mid-Waterproof_Black_03.png',
@@ -181,7 +164,7 @@ INSERT INTO products
   (sku, category_id, style_id, color_id,
     name, description, image1, image2, image3, price, disp)
     VALUES ('1008c03', 1, 1, 3, 'Thermo Akita Mid Waterproof',
-    '',
+    E'Waterproof membrane seals out water and lets moisture escape\nWaterproof coated leather and mesh upper\nMetal hook traditional lace closure\nBellows tongue keeps out debris\nGaiter D-Ring\nBreathable mesh lining\n200 grams lightweight, low bulk insulation keeps you comfortable\nFleece collar\nKinetic Fit™ BASE removable contoured insole for flexible support\nMerrell Air Cushion in the heel absorbs shock and adds stability\nMolded nylon arch shank\nLightweight EVA foam midsole for stability and comfort\nMerrell sticky rubber outsole tuned for winter optimizes traction over cold weather surface conditions\nLug Depth: 4.0mm\nWeight: 2LBS-5OZ/1028.0G',
     '/Men_Boots_Thermo-Akita-Mid-Waterproof_Brown_01.png',
     '/Men_Boots_Thermo-Akita-Mid-Waterproof_Brown_02.png',
     '/Men_Boots_Thermo-Akita-Mid-Waterproof_Brown_03.png',
