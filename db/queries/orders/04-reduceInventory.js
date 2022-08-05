67185cb56eb41de7ac425901284716c388821daf
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-08-05 13:33:23 -0700

    update query to reduce qty in inventory

--- db/queries/orders/04-reduceInventory.js
+++ db/queries/orders/04-reduceInventory.js
@@ -1,8 +1,12 @@
 const reduceInventory = (db, barcode, qty) => {
   return db.query(`
   UPDATE inventory
-  SET quantity = quantity - $1
-  WHERE barcode = $2 AND quantity >= $1
+  SET quantity =
+  CASE
+    WHEN quantity >= $1 THEN quantity - $1
+    ELSE 0
+  END
+  WHERE barcode = $2
   RETURNING *;`, [qty, barcode])
 }
 module.exports = { reduceInventory };
