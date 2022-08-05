c81343b615259a16005834dc5cb2a8f4b184aff4
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-08-04 21:39:59 -0400

    Fix price and add new implementations to handle new orders

--- routes/checkout.js
+++ routes/checkout.js
@@ -8,21 +8,30 @@ const uuid = require('uuid').v4;
 router.use(express.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-module.exports = () => {
+const { addToOrderLines } = require("../db/queries/orders/01-addToOrderLines")
+const { addToOrders } = require("../db/queries/orders/02-addToOrders")
+
+
+module.exports = (db) => {
 
   router.post("/", async (req, res) => {
-    // console.log("body", req.body);
+    console.log("body", req.body);
     let error, status;
 
     try {
       const { cart, token } = req.body;
 
-      const totalEndPrice = cart.reduce((total, item) => {
+      const totalEndPriceBeforeTaxes = cart.reduce((total, item) => {
         const totalPriceOfEachProduct = (item.quantity / 100) * item.price;
         return totalPriceOfEachProduct + total;
       }, 0);
+
+      const taxes = totalEndPriceBeforeTaxes * 0.13;
+      const totalAfterTaxes = (totalEndPriceBeforeTaxes + taxes);
+      console.log("with taxes", totalAfterTaxes.toFixed(2));
       // console.log('total', totalEndPrice);
       // console.log('stripe', stripe)
+
       const customer = await stripe.customers.create({
         email: token.email,
         source: token.id
@@ -31,10 +40,10 @@ module.exports = () => {
       const key = uuid();
       // console.log('token', token);
       // console.log('key', key);
-      console.log('cart', cart);
+      // console.log('cart', cart);
       const charge = await stripe.charges.create(
         {
-          amount: totalEndPrice * 100,
+          amount: Math.floor((totalAfterTaxes * 100).toFixed(2)),
           currency: "cad",
           customer: customer.id,
           receipt_email: token.email,
@@ -56,12 +65,22 @@ module.exports = () => {
       );
       // console.log("Charge:", { charge });
       status = "success";
+
+      const newOrder = await addToOrders(db, charge.amount, req.body.token)
+      // console.log("newLog", newOrder)
+
+      const orderLine = await addToOrderLines(db, req.body.cart[0], newOrder.rows[0].id)
+      console.log("orderLine", orderLine)
+
     } catch (error) {
       console.log("error", error);
       status = "failure";
     }
     res.json({ error, status });
 
+    // router.post("/api/orders", (req, res) => {
+    //   console.log("orders", req)
+    // })
   });
   return router;
 };
