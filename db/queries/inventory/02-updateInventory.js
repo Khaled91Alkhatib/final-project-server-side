37c72ba0d94dfe6726d3c652fa61a283cafa3760
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-08-06 10:34:59 -0700

    fix inventory query bug

--- db/queries/inventory/02-updateInventory.js
+++ db/queries/inventory/02-updateInventory.js
@@ -2,7 +2,7 @@ const updateInventory = (db, barcode, qty) => {
   return db.query(`
   UPDATE inventory
   SET quantity = quantity + $1
-  WHERE barcode = $2 AND quantity >= $1
+  WHERE barcode = $2
   RETURNING *;`, [qty, barcode])
 }
 module.exports = { updateInventory };
